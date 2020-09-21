1edecf974b05c39a442d3e3c2ae9a62610b3f781
alexkishekedu <go9709@wayne.edu>
2020-09-21 16:55:51 -0400

    adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,6 @@
-// Simple C++ program to display "Hello World" 
+// Simple C++ program to display "Hello World"
+// Author: Alex Kishek <go9709@wayne.edu>
+
 // Header file for input output functions 
 #include<iostream>  
   
